fe1663005a18ff15a377178b3e8b01f1396e018d
MichaelChen <helloworldsmart3@gmail.com>
2021-11-18 21:37:47 +0800

    [100-days-of-code]: Day 18

---
+++ 100-days-of-code/day-18-start/main.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+import random
+# import turtle as t
+# import heroes
+# print(heroes.gen())
+
+
+# timmy_the_turtle = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(90)
+
+# for _ in range(15):
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.pendown()
+
+tim = Turtle()
+
+# My answer
+# for i in range(3, 9):
+#     r = 360 / i
+#     for _ in range(i):
+#         tim.forward(100)
+#         tim.right(r)
+
+colours = ["dark slate gray", "royal blue", "firebrick", "medium spring green", "dark magenta", "yellow", "sky blue", "deep pink"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
+
+screen = Screen()
+screen.exitonclick()
+
