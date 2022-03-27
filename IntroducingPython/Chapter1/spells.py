e91a6f29181f8cf721ea25a3605204972dcd0754
MichaelChen <helloworldsmart3@gmail.com>
2022-03-27 21:22:58 +0800

    [IntroducingPython]

---
+++ IntroducingPython/Chapter1/spells.py
@@ -0,0 +1,9 @@
+spells = [
+    "Riddikulus!",
+    "Wingardium Leviosal!",
+    "Avada Kedavral",
+    "Expecto Patronum!",
+    "Nox!",
+    "Lumos!",
+]
+print(spells[3])
---
+++ IntroducingPython/Chapter3/problemSet.py
@@ -0,0 +1,12 @@
+# 3.1, 3.2
+seconds_per_hour = 60 * 60
+# 3600
+
+# 3.3, 3.4
+seconds_per_day = seconds_per_hour * 24
+# 86400
+
+seconds_per_day / seconds_per_hour
+# 24.0
+seconds_per_day // seconds_per_hour
+# 24
