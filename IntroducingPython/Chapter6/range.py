9c7422fde16f086d123834e35023e64850317e5b
MichaelChen <helloworldsmart3@gmail.com>
2023-01-28 14:40:37 +0800

    Feat: IntroducingPython Chapter6 range.py

---
+++ IntroducingPython/Chapter6/range.py
@@ -0,0 +1,14 @@
+for x in range(0, 3):
+    print(x)
+
+array = list(range(0, 3))
+print(array)
+
+for x in range(2, -1, -1):
+    print(x)
+
+array2 = list(range(2, -1, -1))
+print(array2)
+
+array3 = list(range(0, 11, 2))
+print(array3)
